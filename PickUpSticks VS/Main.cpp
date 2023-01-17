73d0d926a18c918b83a5b9ec91c483332681b11c
lam12602 <79139004+lam12602@users.noreply.github.com>
2023-01-17 15:57:55 +0000

    added more sprite changes and started text UI

--- PickUpSticks VS/Main.cpp
+++ PickUpSticks VS/Main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <time.h>
+#include <stdlib.h>
 
 
 int main()
@@ -9,8 +11,13 @@ int main()
 #pragma region SetUp
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pick Up Sticks", sf::Style::None);
 
+    srand(time(NULL));
+
+    
+
     sf::Texture playerTexture;
     sf::Texture grassTexture;
+    sf::Texture stickTexture;
     if (!playerTexture.loadFromFile("Assets/Player_Stand.png"))
     {
         //error
@@ -23,6 +30,12 @@ int main()
 
     if (!grassTexture.loadFromFile("Assets/Grass.png"))
     {
+        
+        std::cout << "load texture failed" << std::endl;
+
+    }
+    if (!stickTexture.loadFromFile("Assets/Stick.png"))
+    {
         std::cout << "load texture failed" << std::endl;
     }
 
@@ -34,14 +47,50 @@ int main()
     sf::Sprite GrassSprite;
     GrassSprite.setTexture(grassTexture);
 
+    sf::Sprite StickSprite;
+    StickSprite.setTexture(stickTexture);
 
 
     std::vector<sf::Sprite> grassSprites;
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < 5; ++i)
     {
+        int colourtint = 100 + rand() % 155;
+        float grassSize = 0 + rand() % 3;
+        GrassSprite.setPosition(sf::Vector2f(rand() % (window.getSize().x) - grassTexture.getSize().x, rand() % (window.getSize().y - grassTexture.getSize().y)));
+        GrassSprite.setColor(sf::Color(colourtint, colourtint, colourtint));
+        GrassSprite.setScale(grassSize, grassSize);
         grassSprites.push_back(GrassSprite);
     }
-    playerSprite.setPosition(sf::Vector2f(100.0f, 100.0f));
+    std::vector<sf::Sprite> StickSprites;
+    StickSprite.setPosition(sf::Vector2f(rand() % (window.getSize().x) - stickTexture.getSize().x, rand() % (window.getSize().y - stickTexture.getSize().y)));
+    int stickrotation = 0 + rand() % 360;
+    StickSprite.setRotation(stickrotation);
+    StickSprites.push_back(StickSprite);
+    
+    
+    playerSprite.setPosition(sf::Vector2f(0.0f, 100.0f));
+    playerSprite.setColor(sf::Color::Cyan);
+    //playerSprite.setRotation(90);
+    //playerSprite.setScale(3.0f, 3.0f);
+    playerSprite.setOrigin(playerTexture.getSize().x / 2, playerTexture.getSize().y / 2);
+    
+    //load fonts
+    sf::Font gameFont;
+    gameFont.loadFromFile("Assets/GameFont.ttf");
+
+
+    //creat text objects
+    sf::Text gameTitle;
+    gameTitle.setFont(gameFont);
+    gameTitle.setString("Pick up Sticks");
+    float textwidth = gameTitle.getLocalBounds().width;
+    gameTitle.setPosition(window.getSize().x / 2.0f - textwidth / 2.0f, 10);
+
+    sf::Text scoreLabel;
+    scoreLabel.setFont(gameFont);
+    scoreLabel.setString("0000");
+    float scorewidth = scoreLabel.getLocalBounds().width;
+    scoreLabel.setPosition();
 
 
 #pragma endregion
@@ -74,12 +123,18 @@ int main()
 
 #pragma region Draw
         window.clear(sf::Color(61, 204, 90));
-        window.draw(playerSprite);
+        
         for (int i = 0;i < grassSprites.size(); ++i)
         {
             window.draw(grassSprites[i]);
+            
         }
-
+        for (int i = 0; i < StickSprites.size(); ++i)
+        {
+            window.draw(StickSprites[i]);
+        }
+        window.draw(playerSprite);
+        window.draw(gameTitle);
         window.display();
     }
 
